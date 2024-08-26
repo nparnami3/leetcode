209f2f110f42861eaeecd73f697c8b675b02807f
Nishtha Parnami <87017035+nparnami3@users.noreply.github.com>
2024-08-26 11:02:00 +0530

    Time: 21 ms (28.64%), Space: 27.1 MB (45.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,24 +1,34 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
+//Optimal Approach:
     int majorityElement(vector<int>& arr) {
-    //size of the given array:
-    int n = arr.size();
-    //declaring a map:
-    map<int, int>mp;
- 
-
-    //storing the elements with its occurnce:
-    for(int i = 0; i < n; i++){
-        mp[arr[i]]++;
-    }
-    
-    //searching for the majority element:
-    for(auto it : mp){
-        if(it.second > (n/2)){
-            return it.first;
+        int n = arr.size();
+     //applying the algorithm:
+        int count = 0;
+        int ele;
+        for(int i = 0; i < n; i++){
+            if(count == 0){
+                count=1;
+                ele = arr[i];
+            }
+            else if(ele == arr[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
         }
+          //checking if the stored element
+         // is the majority element:
+        int count1=0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] == ele ){
+                count1++;
+            }
+        }
+
+        if(count1 > (n/2)) return ele;
+        
+        return -1;
     }
-    return -1;
-}
 };
