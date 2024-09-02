2662b8ab614f8b90b3957214d0405f88c0dd4cfc
Nishtha Parnami <87017035+nparnami3@users.noreply.github.com>
2024-09-02 17:58:15 +0530

    Time: 8 ms (33.63%), Space: 20.7 MB (46.79%) - LeetHub

--- 3220-count-tested-devices-after-test-operations/3220-count-tested-devices-after-test-operations.cpp
+++ 3220-count-tested-devices-after-test-operations/3220-count-tested-devices-after-test-operations.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-//BRUTE FORCE
+//OPTIMAL SOLLUTION
     int countTestedDevices(vector<int>& arr) {
-        int n = arr.size();
-        int count=0;
-        // int i = 0;
-        for(int i=0; i<n; i++){
-            if(arr[i] == 0) {
-                continue;
-            }
-            else if(arr[i] > 0){
-                count++;
-                for(int j=i+1; j<n; j++){
-                    if(arr[j] == 0) continue;
-                    else{
-                    arr[j]= arr[j]-1;
-                    } 
-                }
+        int check=0; // to check
+        int ans=0;
+        int n =arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i] > check){
+                ans++;
+                check++;
             }
         }
-        return count;
+        return ans;
     }
 };
