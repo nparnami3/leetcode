d7bbb6aaec38c6e022680d44f2937dc1aec82d9a
Nishtha Parnami <87017035+nparnami3@users.noreply.github.com>
2025-05-08 05:36:41 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.84%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,7 +3,6 @@ public:
     int singleNumber(vector<int>& arr) {
         int n = arr.size();
         int res = 0;
-        //using XOR method
         for(int i=0; i<n; i++){
             res = res ^ arr[i];
         }
